9d7eb07d2e0d2a9f2d0e41c00fef17106b4f3bed
Mertcan <65860708+Mertcangndn@users.noreply.github.com>
2026-04-18 23:52:22 +0300

    Compile files deleted

--- build/my_py_pkg/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/mertcan/ros2YT_ws/install/my_py_pkg'
--- build/my_py_pkg/share/my_py_pkg/hook/pythonpath_develop.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/hook_prepend_value.sh.em
-
-_colcon_prepend_unique_value PYTHONPATH "/home/mertcan/ros2YT_ws/build/my_py_pkg"
